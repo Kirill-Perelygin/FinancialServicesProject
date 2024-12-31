427dfc745fffb8964ef1121c26db1e929e703b79
Kirill Perelygin <perelygin.kirill@gmail.com>
2024-12-31 23:18:02 +0300

    Continue working on authorization

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,30 @@ import java.util.Scanner;
 
 public class Main extends Users {
 
-
     public static void main(String[] args) {
-        createrUser("Ivan","12345");
-        checkUser("Ivan", "12345");
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Добро пожаловать");
+        System.out.println("Что будем делать?");
+        System.out.println("1. Авторизоваться");
+        System.out.println("2. Регистрироваться");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case (1): {
+                System.out.println("Введите логин");
+                String login = scanner.next();
+                System.out.println("Введите пароль");
+                String password = scanner.next();
+                checkUser(login, password);
+                break;
+            }
+            case (2): {
+                System.out.println("Введите логин");
+                String login = scanner.next();
+                System.out.println("Введите пароль");
+                String password = scanner.next();
+                createrUser(login, password);
+                break;
+            }
+        }
     }
 }
